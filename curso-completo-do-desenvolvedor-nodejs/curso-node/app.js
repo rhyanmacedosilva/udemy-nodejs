140b1583dc68a8d76f9b909823c242cafc5a1a7e
rhyrms <rhyrms@hotmail.com>
2021-08-07 01:28:45 -0300

    Finalizado módulo 4 do curso, refactoring do projeto com módulos.

--- curso-completo-do-desenvolvedor-nodejs/curso-node/app.js
+++ curso-completo-do-desenvolvedor-nodejs/curso-node/app.js
@@ -1,26 +1,14 @@
-let express = require('express');
-let app = express();
+let app = require('./config/server');
 
-app.set('view engine', 'ejs');
+let rotaNoticias = require('./app/routes/noticias');
+rotaNoticias(app);
 
-app.get('/tecnologia', (req, res) => {
-    // res.send('<html><body>Portal Tec</body></html>');
-    res.render('secao/tecnologia');
-});
+let rotaHome = require('./app/routes/home');
+rotaHome(app);
 
-app.get('/', (req, res) => {
-    // res.send('<html><body>Portal News</body></html>');
-    res.render('home/index');
-});
-
-app.get('/formulario-inclusao-noticia', (req, res) => {
-    res.render('admin/form_add_noticia');
-});
-
-app.get('/noticias', (req, res) => {
-    res.render('noticias/noticias');
-});
+let rotaFormInclusaoNoticia = require('./app/routes/formulario-inclusao-noticia');
+rotaFormInclusaoNoticia(app);
 
 app.listen(4100, () => {
-    console.log('server running with express');
+    console.log('server running');
 });
