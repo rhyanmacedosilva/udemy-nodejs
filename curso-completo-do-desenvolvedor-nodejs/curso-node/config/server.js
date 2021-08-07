e47ca7454ca2aa5be544edec5c2801492a8da370
rhyrms <rhyrms@hotmail.com>
2021-08-07 20:30:37 -0300

    Resolvido problema do body-parser depreciado. Ficou srv.use(express.urlencoded({ extended: true })), basicamente foi passado o parâmetro extended.

--- curso-completo-do-desenvolvedor-nodejs/curso-node/config/server.js
+++ curso-completo-do-desenvolvedor-nodejs/curso-node/config/server.js
@@ -3,11 +3,12 @@ const consign = require('consign');
 const { check, validationResult } = require('express-validator');
 
 const srv = express();
+
 srv.set('view engine', 'ejs');
 srv.set('view options', { delimiter: '?' })
 srv.set('views', './app/views');
 
-srv.use(express.urlencoded());
+srv.use(express.urlencoded({ extended: true }));
 srv.use(express.json());
 
 consign()
